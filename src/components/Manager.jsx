8634d017a7bfe2922486d8b005b5b46f29071fc0
collision-hp <champatisatyabrata26@gmail.com>
2025-07-09 11:48:57 +0530

    frontend

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -2,23 +2,35 @@ import React from "react";
 
 const Manager = () => {
   return (
-    <div>
-        <h1 className="text-white text-5xl py-2">PassMan</h1>
-        <p className="text-white text-xl">Your personal password manager</p>
-        <div className=" flex flex-col p-4 items-center">
-          <div className="flex flex-col gap-8 py-5 ">
-          <input className="inpbox" type="text" placeholder="Enter your username"/>
-          <input className="inpbox" type="text" name="" />
-          <input className="inpbox" type="text" name="" />
+    <div className="py-20">
+      <h1 className="text-white text-5xl py- font-serif">PassMan</h1>
+      <p className="text-white text-xl">your personal password manager</p>
+      <div className=" flex flex-col p-4 items-center">
+        <div className="flex flex-col gap-8 py-4 ">
+          <input className="inpbox" type="text" placeholder="Username" />
+          <input className="inpbox" type="text" placeholder="Website Name" />
+          <div className="relative">
+            <input className="inpbox flex-1" type="text" placeholder="Password"/>
+            <span
+              className="absolute right-6 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500"
+              style={{ userSelect: "none" }}
+            >
+              <lord-icon
+              src="https://cdn.lordicon.com/ntfnmkcn.json"
+              trigger="hover">
+            </lord-icon>
+            </span>
           </div>
-          <button className="savebtn text-white ">
-            <lord-icon
-              src="https://cdn.lordicon.com/wixhsrdu.json"
-              trigger="hover"
-            ></lord-icon>
-            Save
-          </button>
         </div>
+        <button className="savebtn text-white ">
+          <lord-icon
+            src="https://cdn.lordicon.com/wixhsrdu.json"
+            className="lrd"
+            trigger="hover"
+          ></lord-icon>
+          Save
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className="bg-white text-black-900 flex justify-between items-center px-4 h-14">
-            <div className='logo'>PassMan</div>
+        <nav className="fixed top-0 left-0 w-full z-50 bg-white text-black-900 flex justify-between items-center py-2 px-4 h-14">
+            <div className='logo'><img src="/logo.png" alt="error" /></div>
             <ul>
                 <li className='gitlogo'>
                     <a href="https:/github.com"><img src="/gitlogo.png" alt="git" /></a>
