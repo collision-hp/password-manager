29cd2a5b22579b611563c60b2cf15da8c70d4abe
collision-hp <champatisatyabrata26@gmail.com>
2025-07-15 23:19:20 +0530

    Password

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -3,13 +3,12 @@ import React, { useEffect, useState } from "react";
 const Manager = () => {
   const [form, setform] = useState({ site: "", username: "", password: "" });
   const [passwordArray, setpasswordArray] = useState([]);
-  useEffect(()=>{
+  useEffect(() => {
     let passwords = localStorage.getItem("password");
     if (passwords) {
       setpasswordArray(JSON.parse(passwords));
     }
-    
-  },[])
+  }, []);
 
   const [iconProps, setIconProps] = useState({
     //iconProps is the current state value (an object holding your icon's properties).
@@ -29,16 +28,16 @@ const Manager = () => {
   };
 
   const savePass = () => {
-    setpasswordArray([...passwordArray,form])
-    localStorage.setItem("password",JSON.stringify([...passwordArray,form]))
-    console.log([...passwordArray,form])
+    setpasswordArray([...passwordArray, form]);
+    localStorage.setItem("password", JSON.stringify([...passwordArray, form]));
+    console.log([...passwordArray, form]);
   };
   const handleChange = (e) => {
     setform({ ...form, [e.target.name]: e.target.value });
   };
 
   return (
-    <div className="py-20">
+    <div className="py-24">
       <h1 className="text-white text-5xl font-serif">PassMan</h1>
       <p className="text-white text-xl">your personal password manager</p>
       <div className="flex flex-col p-4 items-center">
@@ -84,7 +83,7 @@ const Manager = () => {
           </div>
         </div>
 
-        <button onClick={savePass} className="savebtn text-white ">
+        <button onClick={savePass} className="savebtn text-white">
           <lord-icon
             src="https://cdn.lordicon.com/wixhsrdu.json"
             className="lrd"
@@ -92,6 +91,31 @@ const Manager = () => {
           ></lord-icon>
           Save
         </button>
+      </div>
+
+      <div className="password py-7">
+        <h2 className="text-2xl font-bold text-white py-3">Your Credentials</h2>
+        {passwordArray.length=== 0 && <div className="text-lg text-white">No passwords to show</div>}
+        {passwordArray.length !==0 &&
+        <table className="table-auto mx-auto w-full max-w-3xl text-center text-white rounded-xl overflow-hidden">
+          <thead className="bg-green-700">
+            <tr>
+              <th className="px-20 py-2 text-lg">Username</th>
+              <th className="px-20 text-lg">Website URL</th>
+              <th className="px-20 text-lg">Password</th>
+            </tr>
+          </thead>
+          <tbody className="tbody">
+            {passwordArray.map((item,index)=>{
+
+              return <tr key={index}>
+              <td className="py-2 border border-white">{item.site}</td>
+              <td className="py-2 border border-white">{item.username}</td>
+              <td className="py-2 border border-white">{item.password}</td>
+            </tr>
+            })}
+          </tbody>
+        </table>}
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className="fixed top-0 left-0 w-full z-50 bg-white text-black-900 flex justify-between items-center py-2 px-4 h-14">
+        <nav className="fixed top-0 left-0 w-full bg-white text-black-900 flex justify-between items-center px-4 h-14">
             <div className='logo'><img src="/logo.png" alt="error" /></div>
             <ul>
                 <li className='gitlogo'>
